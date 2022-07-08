1a06d846bc0a2b98cba1558309f294658346096b
Erdem Yilmaz <eryilmaz@k2m15s07.42kocaeli.com.tr>
2022-07-08 10:52:41 +0300

    ilk daha bitmedi temeler atıdı :)

---
+++ lib/push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eryilmaz <eryilmaz@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 10:43:05 by eryilmaz          #+#    #+#             */
+/*   Updated: 2022/07/08 10:43:06 by eryilmaz         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+typedef struct push_swap
+{
+	struct	push_swap next;
+	int		number;
+}	push_swap;
+
+#endif
